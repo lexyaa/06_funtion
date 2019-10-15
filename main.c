c2ce1b0fdf682029783e16927f7791fb68bc6c75
lexyaa <55127454+lexyaa@users.noreply.github.com>
2019-10-15 16:12:08 +0900

    06_07

--- main.c
+++ main.c
@@ -3,16 +3,34 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int square(int n);
+int factorial(int n)
+{
+	int res=1;
+	int i;
+	for(i=1;i<=n;i++)
+	{
+		res = res*i; 
+	}
+	
+	return res;
+}
+
+int calcombination(int n, int r)
+{
+	int numerator, denominator;
+	numerator = factorial(n);
+	denominator = factorial(n-r)*factorial(r);
+	return (numerator/denominator);
+}
 
 int main(void)
 {
-	int result;
-	result = square(5);
-	printf("%d", result);
-}
-
-int square(int n)
-{
-	return (n*n);
+	int n, r;
+	int res;
+	printf("input n and r :");
+	scanf("%d %d", &n, &r);
+	res = calcombination(n, r);
+	printf("combination result is %d\n", res);
+	
+	return 0;
 }
